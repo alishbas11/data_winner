ee50db9515d32758395a1104517df21f65886b17
alishbas11 <alishbas809@gmail.com>
2025-09-14 12:41:07 +0500

    Day1_grocery_bill.py

--- Day1_grocery_bill.py
+++ Day1_grocery_bill.py
@@ -14,11 +14,9 @@ if sum >1000:
     print("your price after discount is: ",discount)
 else:
     print("your total price is: ",shopping_list)
-# There is no need to call 'init' from mimetypes or use 'git' here.
-# If you want to save your project using git, you can run the following commands in your terminal:
-# git add .
-# git commit -m "Save project"
-# git push;
+
+
+print("Happy Shopping")
 
 
     
